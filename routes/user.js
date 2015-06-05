46d57c07746a2610513e7a1408a5bcb8fa709212
dmartin <umdjm@hotmail.com>
2015-06-05 16:36:06 -0400

    add cards to user

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var userDM = require('../data/user');
+var userService = require('../data/user');
+var portfolioService = require('../data/portfolio');
 
 router.get('/', function(req, res) {
-  var user = userDM.getById(2)
+  var user = userService.getById(2)
     .then(function(users){
       var user = users[0];
-      res.render('user', { username: user.username, title: 'Users' });
+
+      portfolioService.getPortfolio(2)
+        .then(function(cards){
+          user.cards = cards;
+          res.render('user', { user: user, title: 'Users' });
+        });
+
     });
 });
 
