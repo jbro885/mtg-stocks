46378f053c46203df60bdbabd06e8710cfc902b9
Ron Emaus <remaus@acm.org>
2015-06-05 12:45:27 -0400

    basic jasmine test for getPortfolio

---
+++ spec/portfolio-service-spec.js
@@ -0,0 +1,23 @@
+/**
+ * Created by remaus on 6/5/2015.
+ */
+var user = require('../data/portfolio');
+describe("A portfolio", function() {
+  it("it exists", function() {
+    var results;
+
+    waitsFor( function()
+    {
+      return result;
+    });
+
+
+    expect(results.length).toBeGreaterThan(1);
+
+
+    portfolio.getPortfolio(1).then(function(){
+
+    });
+
+  });
+});
