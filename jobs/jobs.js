8f81a8cc9ddd75032e20474f8b3d7acf513a78b7
John Fonte <john@fonte.com>
2015-06-05 13:12:42 -0400

    add imagescraper job.

--- jobs/jobs.js
+++ jobs/jobs.js
@@ -1,5 +1,11 @@
 require("coffee-script/register");
 var tcg = require('./scrapers/tcgscraper.coffee');
+var imageJob = require('./scrapers/imagescraper.coffee');
 
 tcg()
   .then(console.log('done'));
+
+url = "http://shop.tcgplayer.com/magic/collector's-edition/zombie-master-(ce)";
+
+imageJob(url)
+  .then(console.log)
