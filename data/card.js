c03aaf6e4dacd58e49eaa2f4e60999d9ddc1b937
Derek <dereknelson01@gmail.com>
2015-06-05 11:21:27 -0400

    add filepicker to config

---
+++ data/card.js
@@ -0,0 +1,34 @@
+var db = require('./database');
+
+var card = function(){
+  var addCard = function(name, password){
+    var connection = db();
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+      if (err) throw err;
+
+      console.log('The solution is: ', rows[0].solution);
+    });
+
+    connection.end();
+
+  };
+
+  var getCard = function(id){
+    var connection = db();
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+      if (err) throw err;
+
+      console.log('The solution is: ', rows[0].solution);
+    });
+
+    connection.end();
+  };
+
+  return {
+
+  }
+};
