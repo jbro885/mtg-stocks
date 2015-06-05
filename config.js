e916a4d03a89af1dc8c417bc5a19c0edcdba9dee
Derek <dereknelson01@gmail.com>
2015-06-05 10:07:10 -0400

    Added config

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mysql : {
+    user : process.env.MYSQL_USER,
+    pass : process.env.MYSQL_PASS,
+    url : process.env.MYSQL_URL,
+    database : process.env.MYSQL_DATABASE
+  }
+};
