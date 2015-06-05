d10141bb801755406173eeedbdf49331ae8c4e04
Ron Emaus <remaus@acm.org>
2015-06-05 14:13:35 -0400

    each method requires a separate connection in order to run multiple tests

--- data/portfolio.js
+++ data/portfolio.js
@@ -6,36 +6,69 @@ var Promise = require('bluebird');
 
 
 var portfolio = function(){
-  var connection = db();
-  connection.connect();
 
   var addCard = function(userid, cardname, quantity){
-
+    var connection = db();
+    connection.connect();
+    var defer = Promise.pending();
     //get quantity of this card in the user's existing portfolio
     //need to add to existing portfolio for this card
     connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-      if (err) throw err;
-
-      console.log('The quantity is: ', rows[0].solution);
+      if (!err) {
+        console.log('You now have ' + rows[0].solution);
+        defer.fulfill({'magic_add':5});
+      } else {
+        defer.reject(err);
+        console.log('Error while performing Query.');
+      }
     });
 
     connection.end();
-
+    return defer.promise;
   };
 
   var getCard = function(userid, cardname){
+    var connection = db();
+    connection.connect();
+    var defer = Promise.pending();
     //get quantity of this card in the user's existing portfolio
     connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-      if (err) throw err;
-
-      console.log('You own this number of ' + cardname + ' ' + rows[0].solution);
+      if (!err) {
+        console.log('You now have ' + rows[0].solution);
+        defer.fulfill({'magic_get':6});
+      } else {
+        defer.reject(err);
+        console.log('Error while performing Query.');
+      }
     });
 
     connection.end();
-    return {cardname:1};
+    return defer.promise;
   };
 
+  var removeCard = function(userid, cardname, quantity){
+    var connection = db();
+    connection.connect();
+    var defer = Promise.pending();
+
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+      if (!err) {
+        console.log('You now have ' + rows[0].solution);
+        defer.fulfill({'magic_remove':7});
+      } else {
+        defer.reject(err);
+        console.log('Error while performing Query.');
+      }
+    });
+
+    connection.end();
+    return defer.promise;
+  };
+
+
   var getPortfolio = function(userid){
+    var connection = db();
+    connection.connect();
     //get quantity of each card in the user's portfolio
     var defer = Promise.pending();
 
@@ -54,18 +87,28 @@ var portfolio = function(){
   };
 
   var buyCard = function(userid, cardname, quantity){
+    var connection = db();
+    connection.connect();
+    var defer = Promise.pending();
     //get quantity of this card in the user's existing portfolio
     connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-      if (err) throw err;
-
-      console.log('You own ' + rows[0].solution);
+      if (!err) {
+        console.log('You now have ' + rows[0].solution);
+        defer.fulfill({'magic_remove':7});
+      } else {
+        defer.reject(err);
+        console.log('Error while performing Query.');
+      }
     });
 
     connection.end();
-    return {'requested':quantity, 'bought':quantity};
+    return defer.promise;
   };
 
   var sellCard = function(userid, cardname, quantity){
+    var connection = db();
+    connection.connect();
+    var defer = Promise.pending();
     //do you own the card? how many? cap sell at user owned quantity.
     //get the price
     //compute sold dollar amount
@@ -73,17 +116,22 @@ var portfolio = function(){
     //update user balance
 
     connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-      if (err) throw err;
-
-      console.log('You own ' + rows[0].solution);
+      if (!err) {
+        console.log('You now have ' + rows[0].solution);
+        defer.fulfill({'magic_remove':7});
+      } else {
+        defer.reject(err);
+        console.log('Error while performing Query.');
+      }
     });
 
     connection.end();
-    return {'requested':quantity, 'sold':quantity};
+    return defer.promise;
   };
 
   return {
     addCard: addCard,
+    removeCard: removeCard,
     getCard: getCard,
     getPortfolio: getPortfolio,
     buyCard: buyCard,
--- spec/profile-test.js
+++ spec/profile-test.js
@@ -1,6 +1,17 @@
 var portfolio = require('../data/portfolio');
+var promise = require('bluebird');
 
-portfolio.getPortfolio(1)
-  .then(function(rows){
-  console.log(rows);
-});
+promise.all([
+  portfolio.addCard(1, 'queen', 3)
+    .then(function (rows) {
+      console.log(rows);
+    }),
+  portfolio.getCard(1, 'king')
+    .then(function(rows){
+      console.log(rows);
+    }),
+  portfolio.getPortfolio(1)
+    .then(function (rows) {
+      console.log(rows);
+    })
+]);
