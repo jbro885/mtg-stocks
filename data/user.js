dc5a6340a40a1125446a407a098aca994350e5dc
mkouba10 <michelle.kouba@learninga-z.com>
2015-06-05 14:47:32 -0400

    updating getById to return the username

--- data/user.js
+++ data/user.js
@@ -2,27 +2,30 @@ var db = require('./database');
 var Promise = require('bluebird');
 
 var user = function(){
+
   var addUser = function(name, password){
+    var defer = Promise.pending();
     var connection = db();
     connection.connect();
-    connection.query('INSERT INTO user (username, user_password) VALUES (', function(err, rows, fields) {
+    var data = [{'username':name},{'password':password},{'balance':50}];
+    connection.query('INSERT INTO user SET ?', data,  function(err, result) {
       connection.end();
       if (!err) {
-        defer.fulfill(rows);
+        defer.fulfill(result);
       } else {
         defer.reject(err);
         console.log('Error while performing Query.');
       }
     });
 
+    return defer.promise;
   };
 
   var getById = function(id){
     var defer = Promise.pending();
-
     var connection = db();
     connection.connect();
-    var queryString = "SELECT username from user where user_id="+id;
+    var queryString = "SELECT username, balance from user where user_id="+id;
     connection.query(queryString, function(err, rows, fields) {
       connection.end();
       if (!err) {
