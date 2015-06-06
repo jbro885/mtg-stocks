411ea56aad928f94766119c19a26697a7b68f20b
Derek <dereknelson01@gmail.com>
2015-06-06 11:17:16 -0400

    change name of portfolio

--- spec/portfolio-service-spec.js
+++ spec/portfolio-service-spec.js
@@ -1,10 +1,20 @@
 /**
  * Created by remaus on 6/5/2015.
  */
-var user = require('../data/portfolio');
+var portfolio = require('../data/portfolio');
 describe("A portfolio", function() {
   it("it exists", function(done) {
 
     done();
+  });
+
+  it("it exists", function(done) {
+
+    user.getUser(2)
+      .then(function(results){
+        expect(results.user_id).toEqual(2);
+        done();
+      });
+
   });
 });
