8970bb7ebffb0f017d5363774ef76cb6ed67e4c4
Derek <dereknelson01@gmail.com>
2015-06-05 21:56:46 -0400

    add leaders controller

---
+++ routes/leaders.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+
+  var user = userService.getById(2)
+    .then(function(users){
+      var user = users[0];
+
+      portfolioService.getPortfolio(2)
+        .then(function(cards){
+          user.cards = cards;
+          res.render('leaders', { user: user, title: 'MTG' });
+        });
+
+    });
+  res.render('leaders', {})
+
+});
