76647815aed1705a5e31f8117f55ef46aefc4d12
Derek Nelson <dereknelson01@gmail.com>
2015-06-07 02:36:32 -0400

    Fix leaders page

--- routes/leaders.js
+++ routes/leaders.js
@@ -4,7 +4,9 @@ var userService = require('../data/user');
 var promise = require('bluebird');
 
 router.get('/leaders', function(req, res) {
-  res.render('leaders', { user: req.user, leaderList: leaders, title: 'MTG' });
+  userService.getLeaders().then(function(leaders){
+    res.render('leaders', { user: req.user, leaderList: leaders, title: 'MTG' });
+  });
 });
 
 module.exports = router;
